a63e4b0e41eaebf63039aacbf61b5e2cfd9cc573
andrei_kostin <akostin84@yandex.com>
2022-11-20 09:59:53 +0300

    test

---
+++ test.java
@@ -0,0 +1,11 @@
+/**
+* to run test.java - type java test.java
+*/
+public class test{
+    public static void main(String[] args) {
+        int a = 123;
+        a = a-- - --a;
+        System.out.println(a);
+    }
+ }
+ 
