05c3a15c923813578323feffac8a578f0ee9787d
Andrei K <akostin84@yandex.com>
2022-12-30 14:53:07 +0300

    project

--- project.java
+++ project.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class project {
     public static void main(String[] args) {
@@ -20,12 +21,85 @@ public class project {
 
 
         var notebooks = new HashSet<Notebook>(Arrays.asList(n1, n2, n3, n4, n5));
-        //System.out.println(notebooks.size());
         
-        for (Notebook n : notebooks) {
-            System.out.println(n.getMemory());
-        }
+        Scanner reader = new Scanner(System.in);
 
+        String message = "Select filter:\n 1 - Operating System\n 2 - Disk Size\n 3 - Memory size\n 4 - Color";
+        
+        int filter = readIntFromTerminal(message, reader);
+
+        switch (filter){
+            case 1:
+                filterOS(notebooks, reader);
+                break;
+            case 4:
+                filterColor(notebooks, reader);
+                break;
+            case 2:
+                filterDiskSize(notebooks, reader);
+                break;
+            case 3:
+                filterMemorySize(notebooks, reader);
+                break;
+            default:
+                System.out.println("Wrong number");
+                break;
+        }
     }
-    
+
+    static int readIntFromTerminal(String message, Scanner reader) {
+        System.out.println(message);
+        String strNumber = reader.nextLine(); 
+        int Number = Integer.parseInt(strNumber);
+        return Number;
+    }
+
+
+    static String getFilter(String message, Scanner reader){
+        System.out.println(message);
+        String operation = reader.nextLine();
+        return operation;
+    }
+
+
+    static void filterOS(HashSet<Notebook> notebooks, Scanner reader){
+        String UserSystem = getFilter("Enter Operating System:", reader);
+        for (Notebook n : notebooks) {
+            if (UserSystem.equals(n.getOperatingSystem())) {
+                System.out.println(n);
+            }
+        }
+    }
+
+
+    static void filterColor(HashSet<Notebook> notebooks, Scanner reader){
+        String UserColor= getFilter("Enter Color:", reader);
+        for (Notebook n : notebooks) {
+            if (UserColor.equals(n.getColor())) {
+                System.out.println(n);
+            }
+        }
+    }
+
+
+    static void filterDiskSize(HashSet<Notebook> notebooks, Scanner reader){
+        int UserMin = readIntFromTerminal("Enter Min Disk Size:", reader);
+        int UserMax = readIntFromTerminal("Enter Max Disk Size:", reader);
+        for (Notebook n : notebooks) {
+            if (n.getHardDrive() >= UserMin && n.getHardDrive() <= UserMax) {
+                System.out.println(n);
+            }
+        }
+    }
+
+
+    static void filterMemorySize(HashSet<Notebook> notebooks, Scanner reader){
+        int UserMin = readIntFromTerminal("Enter Min Memory Size:", reader);
+        int UserMax = readIntFromTerminal("Enter Max Memory Size:", reader);
+        for (Notebook n : notebooks) {
+            if (n.getMemory() >= UserMin && n.getMemory() <= UserMax) {
+                System.out.println(n);
+            }
+        }
+    }
 }
