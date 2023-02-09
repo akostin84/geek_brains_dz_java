3a59029f37b622e565b6f6cbf935c2ddbbf0fd1e
akostin84 <kostin.andrew@gmail.com>
2023-02-09 11:27:45 +0300

    final

--- oop_project.java
+++ oop_project.java
@@ -13,18 +13,50 @@ public class oop_project {
         "Select mode:", "1 - Load the whole calendar", "2 - Load the selected day", 
         "3 - Add new note"); 
         System.out.println(initialMsg);
-        int userMode = readIntFromTerminal("You select:");
-        Calendar c = new Calendar("calendar.txt");
-        System.out.println(c);  
+        Scanner reader = new Scanner(System.in);
+        int userMode = readIntFromTerminal("You select:", reader);
+        //switch case
+        switch (userMode) {
+            case  (1):
+                Calendar c = new Calendar("calendar.txt");
+                System.out.println(c); 
+                break;
+            case (2):
+                String userDay1 = readStrFromTerminal("Enter day [dd-MM-yyyy]: ", reader);
+                Calendar c1 = new Calendar("calendar.txt");
+                ArrayList<Event> e = c1.events;
+                for (int i = 0; i < e.size(); i++)
+                    if (e.get(i).isUserDay(userDay1)){
+                        System.out.println(e.get(i));
+                    }  
+                break;
+            case (3):
+                String userDay = readStrFromTerminal("Enter day [dd-MM-yyyy]: ", reader);
+                String userTime = readStrFromTerminal("Enter time [HH:mm:ss]: ", reader);
+                String userName = readStrFromTerminal("Enter name: ", reader);
+                Event newEvent = new Event(userDay, userTime);
+                newEvent.setName(userName);
+                newEvent.save();
+                break;
+            default:
+                System.out.println("Wrong selection");
+                break;
+        }
+        reader.close();
+ 
     }
 
-    static int readIntFromTerminal(String message) {
-        Scanner reader = new Scanner(System.in);
+    static int readIntFromTerminal(String message, Scanner reader) {
         System.out.print(message);
         String strNumber = reader.nextLine(); 
         int Number = Integer.parseInt(strNumber);
-        reader.close();
         return Number;
+    }
+
+    static String readStrFromTerminal(String message, Scanner reader) {
+        System.out.print(message);
+        String strUser = reader.nextLine();
+        return strUser;
     }
 }
 
@@ -32,13 +64,10 @@ interface Saveable {
     void save();
 }
 
-interface Loadable {
-    void load();
-}
-
 class Calendar{
     public ArrayList<Event> events = new ArrayList<Event>();
 
+    // создание календаря из содержимого файла
     public Calendar(String path){
         ArrayList<String> eventsStr = readContent(path);
         for (int i = 0; i < eventsStr.size(); i++){
@@ -51,7 +80,7 @@ class Calendar{
             this.events.add(e);
         }  
     };
-
+    // построчный вывод для чтения содержимого календаря
     @Override
     public String toString(){
         String output = "";
@@ -60,7 +89,7 @@ class Calendar{
         }
         return output;            
     }
-
+    // чтение файла календаря как список строк
     private ArrayList<String> readContent(String path){
         ArrayList<String> words = new ArrayList<String>();
         BufferedReader reader = null;
@@ -81,12 +110,12 @@ class Calendar{
         }
         return words;
     }
-
 }
 
+// класс для единичной записи с указанием времени и заголовка
 class Event implements Saveable{
-    private Date EventTime;
-    private String Name;
+    private Date EventTime; // время события
+    private String Name; // сущность события
     private static final String PATH = "calendar.txt";
     
     public Event(String day, String time){
@@ -100,15 +129,37 @@ class Event implements Saveable{
         }
     }
 
+    // проверка, что событие произошло в указанный день
+    public boolean isUserDay(String user_day){
+        String start_user_day = user_day + "; 00:00:00";
+        String end_user_day = user_day + "; 23:59:00";
+        SimpleDateFormat sfd = new SimpleDateFormat("dd-MM-yyyy; HH:mm:ss");
+        Date start_day = new Date();
+        Date end_day = new Date();
+        try {
+            start_day = sfd.parse(start_user_day);
+            end_day = sfd.parse(end_user_day);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        if (end_day.after(this.getTime()) && start_day.before(this.getTime())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // при выводе на экран для пользователя добавляются поля Date: и Name:
     @Override
     public String toString(){
         SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy; HH:mm:ss");
         String output = String.format("Date: %s Name: %s\n", df.format(EventTime), Name);
         return output;            
     }
-
+    // при записи в файл поля разделяются слешем /
     public String toFile(){
-        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy; HH:mm:ss");
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy/HH:mm:ss");
         String output = String.format("%s/%s", df.format(EventTime), Name);
         return output;            
     }
@@ -124,7 +175,7 @@ class Event implements Saveable{
     public String getName(){
         return this.Name;
     }
-
+// метод для сохранения нового события
     public void save(){
         try (FileWriter writer = new FileWriter(PATH, true)) {
             writer.append(String.format("%s\n", this.toFile()));
